29d4c403a6fd468e62af1aac466a19a7aadee034
若水 <ruoshui0014@outlook.com>
2025-06-28 16:58:18 +0800

    优化线程池任务调度，将调度由server线程执行

--- test/main.cpp
+++ test/main.cpp
@@ -19,11 +19,12 @@ int main(int argc, char* argv[]) {
   LOG_INFO("Proccess entry ...");
 
   base::ThreadPool pool;
-  pool.Start({20, 8});
+  pool.Start({20, 20});
 
   std::mutex mutex;
-  for (int i = 0; i < 40; i++) {
-    pool.AddTask(
+  auto start = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < 10000; i++) {
+    pool.PostTask(
         [&](int id) {
           LOG_DEBUG(std::format("{:0>3}->{}", id, "Thread task..."));
 
@@ -31,18 +32,22 @@ int main(int argc, char* argv[]) {
           auto value = fibonacci(40);
           auto end = std::chrono::high_resolution_clock::now();
           auto duration =
-              std::chrono::duration_cast<std::chrono::seconds>(end - start);
+              std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
           {
             std::lock_guard lock(mutex);
-            std::cout << std::format("{:0>3} fibonacci = {}, time {}s", id,
+            std::cout << std::format("{:0>3} fibonacci = {}, time {}ms", id,
                                      value, duration.count())
                       << std::endl;
           }
         },
         i);
   }
+  auto end = std::chrono::high_resolution_clock::now();
+  auto duration =
+              std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  LOG_DEBUG(std::format("Post tasks all time {}ms.", duration.count()));
 
-  // std::this_thread::sleep_for(std::chrono::seconds(5));
+  std::this_thread::sleep_for(std::chrono::seconds(5));
 
   return 0;
 }
